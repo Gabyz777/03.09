23f617820905626bea7d878ac3ab71891847ea85
Gabyz777 <gabriellydiasmendes@gmail>
2026-09-03 11:52:45 -0300

    commit1

---
+++ buscando-dados/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import ItemCardapio from './components/ItemCardapio.jsx';
+
+function App() {
+    const [cardapio, setCardapio] = useState([]);
+
+    useEffect(() => {
+        console.log("Conectando ao servidor...");
+        setTimeout(() => {
+            setCardapio([
+                { id: 101, nome: "Combo Master", descricao: "Dois lanches + refri 2L", preco: 65.00 },
+                { id: 102, nome: "Hambúrguer de Grão de Bico", descricao: "Opção Vegana", preco: 28.00 },
+                { id: 103, nome: "Açaí na Tigela", descricao: "500ml com morango e leite condensado", preco: 18.00 }
+            ]);
+        }, 2000);
+    }, []);
+
+    return (
+        <div>
+            <h1>Senai Delivery</h1>
+            {cardapio.length === 0 && <h2>🔄 Carregando restaurante...</h2>}
+            {cardapio.map((item) => (
+                <ItemCardapio
+                    key={item.id}
+                    nome={item.nome}
+                    descricao={item.descricao}
+                    preco={item.preco}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default App;
