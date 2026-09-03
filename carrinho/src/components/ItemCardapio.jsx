0ded0d9f57e0e24c78f1b584df9f3edf1cc4af29
Gabyz777 <gabriellydiasmendes@gmail>
2026-09-03 09:00:59 -0300

    commit

--- carrinho/src/components/ItemCardapio.jsx
+++ carrinho/src/components/ItemCardapio.jsx
@@ -3,7 +3,8 @@ function ItemCardapio({ nome, descricao, preco, adicionarItem }) {
         <div>
             <h3>{nome}</h3>
             <p>{descricao}</p>
-            <div style={{ fontWeight: 'bold', color: 'green' }}>R$ {preco.toFixed(2).replace('.', ',')}</div>
+            <div style={{ fontWeight: 'bold', color: 'green' }}>
+                R$ {preco.toFixed(2).replace('.', ',')}</div>
             <button onClick={adicionarItem}>+ Adicionar</button>
         </div>
     );
